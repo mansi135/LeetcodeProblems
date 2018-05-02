393ec36494df4fb6ff9695c0158a9b8bac4ff24e
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2018-05-02 16:37:16 -0700

    rotate in n time and const space

--- Arrays_and_Numbers/189-RotateArray.py
+++ Arrays_and_Numbers/189-RotateArray.py
@@ -22,6 +22,8 @@
 
 
 class Solution(object):
+
+    # Method-1 O(n) time, O(n) space
     def rotate(self, nums, k):
         """
         :type nums: List[int]
@@ -41,5 +43,33 @@ class Solution(object):
 
         nums[:] = new_array[:]
 
-        # One-liner-method-2
+        # One-liner-method-2 -> O(n) time , O(n) space
         # nums[:] = nums[len(nums)-k:] + nums[:len(nums)-k]
+
+        # Method-3 Rotate one by one: O(1) space, but O(n*k) time
+
+        # Method-4 GCD Juggling Algorithm
+
+
+    #Method-5 reverse
+    # In this method , we reverse the half of lists first and then reverse the entire list
+    # O(n) time and O(1) space
+
+    def reverse(self, nums, l, r):
+
+        while l < r:
+            tmp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = tmp
+            l += 1
+            r -= 1
+
+    def rotate_alternate(self, nums, k):
+
+        n = len(nums)
+        k = k % n
+
+        self.reverse(nums, 0, n - k - 1) #O(n) time
+        self.reverse(nums, n - k, n - 1) #O(n) time
+
+        self.reverse(nums, 0, n - 1) #O(n) time
